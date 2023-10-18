c240c94635c459902ab4b4bc365d60c8deb07a13
Vishnuust <vishnu.thulaseedharanpillai@ust.com>
2023-10-18 17:46:12 +0530

    First Assignment Submission

---
+++ Assignment-18-10-23/CalculateAverage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_18_10_23
+{
+    internal class CalculateAverage
+    {
+        int  markone,marktwo,markthree,average,totalmark;
+        string? name;
+
+        public CalculateAverage(int markone, int marktwo, int markthree, string? name)
+        {
+            this.markone = markone;
+            this.marktwo = marktwo;
+            this.markthree = markthree;
+            this.name = name;
+        }
+
+        public double CalculateAverageMark()
+        {
+
+            double average;
+            average=(markone+marktwo+markthree)/2;
+            return average;
+
+        }
+        public double TotalMark()
+        {
+            double total;
+            total=markone+marktwo+markthree;
+            return total;
+        }
+        public char GetGrade()
+        {
+            double average=CalculateAverageMark();
+            if(average<=50)
+            {
+                return 'E';
+            }
+            else if(average>50 && average<=60)
+            {
+                return 'D';
+            }
+
+           else if(average>60 && average<=70)
+            {
+                return 'C';
+            }
+            else if(average>70 && average<=80)
+            {
+                return 'B';
+            }
+            else
+            {
+                return 'A';
+            }
+
+
+        }
+    }
+}
--- Assignment-18-10-23/Program.cs
+++ Assignment-18-10-23/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Assignment_18_10_23;
+
+Console.WriteLine("Enter the Name");
+string name=Console.ReadLine();
+Console.WriteLine("Enter the marks");
+int markone=Convert.ToInt32(Console.ReadLine());
+int marktwo=Convert.ToInt32(Console.ReadLine());
+int markthree=Convert.ToInt32(Console.ReadLine());
+CalculateAverage calculateAverage=new CalculateAverage(markone, marktwo, markthree,name);
+Console.WriteLine($"Name Of Student is: {name}");
+Console.WriteLine($"Average Mark is:{calculateAverage.CalculateAverageMark()}");
+Console.WriteLine($"Total Mark is:{calculateAverage.TotalMark()}");
+Console.WriteLine($"Grade is:{calculateAverage.GetGrade()}");
+
