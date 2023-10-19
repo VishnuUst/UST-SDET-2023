1b33eadb6de4bf9646709da58b3c5054caa19bfb
Vishnuust <vishnu.thulaseedharanpillai@ust.com>
2023-10-19 10:26:48 +0530

    Daily work submission

---
+++ Basic Program/Employees.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Employees
+    {
+       private  int employeeid;
+       private string? employeename, department;
+       private double basicpay;
+
+        public Employees(int employeeid, string? employeename, string? department, double basicpay)
+        {
+            Employeeid = employeeid;
+            Employeename = employeename;
+            Department = department;
+            Basicpay = basicpay;
+        }
+
+        public int Employeeid { get => employeeid; set => employeeid = value; }
+        public string? Employeename { get => employeename; set => employeename = value; }
+        public string? Department { get => department; set => department = value; }
+        public double Basicpay { get => basicpay; set => basicpay = value; }
+
+        public double CalculateSalary()
+        {
+            double grosssalary,netsalary;
+            double allowances = Basicpay * 0.3 + Basicpay * 0.2 + Basicpay * 0.15;
+            grosssalary = Basicpay + allowances  ;
+            double deductions = Basicpay * 0.1;
+            netsalary = grosssalary - deductions;
+
+            return netsalary ;
+        }
+    }
+}
--- Basic Program/Program.cs
+++ Basic Program/Program.cs
@@ -1,9 +1,18 @@
 ﻿
 using Basic_Program;
-Electricity electricity = new Electricity(1234,2400,25000,"ancd");
+
+Employees employees = new Employees(111,"Abc","IT",10000);
+Console.WriteLine($"Employee Id Is: {employees.Employeeid}");
+Console.WriteLine($"Employee Name Is: {employees.Employeename}");
+Console.WriteLine($"Employee Department Is: {employees.Department}");
+Console.WriteLine($"Employee Basic Pay Is: {employees.Basicpay}");
+Console.WriteLine($"Employee Net Salary Is: {employees.CalculateSalary()}");
+
+
+/*Electricity electricity = new Electricity(1234,2400,25000,"ancd");
 double billamount=electricity.CalculateBill();
 Console.WriteLine($"Electricity bill is=- {billamount}");
-/*
+
 Calculation calculation = new Calculation();
 int numberone, numbertwo;
 Console.WriteLine("Enter the two number");
