43e33ed2e64ee4358c834ff01613f2969c302926
Vishnuust <vishnu.thulaseedharanpillai@ust.com>
2023-11-02 16:52:50 +0530

    Assignment two submission on 2-11-2023

--- Assignment-18-10-2023/Program.cs
+++ Assignment-18-10-2023/Program.cs
@@ -1,9 +1,24 @@
 ﻿using Assignment;
-using Assignment_2_oct19_Q1;
 using Assignments;
 using System.Threading.Channels;
 using static Assignments.ExceptionHandling.MyException;
-Hotel hotel = new Hotel("name", 4, "ars", "adfgas", 45);
+TourPackage package = new TourPackage(1, "kochi", "12-11-2024", 1300);
+TourPackage package1 = new TourPackage(2, "Kolam", "12-10-2024", 1200);
+TourPackage package2 = new TourPackage(3, "kannur", "2-2-2023", 1100);
+
+TourPackage.TourPackages.Add(package1);
+TourPackage.TourPackages.Add(package2);
+TourPackage.TourPackages.Add(package);
+
+
+Thread thread = new Thread(TourPackage.HotelReservation);
+
+Thread thread1 = new Thread(TourPackage.HotelReservation);
+
+thread1.Start();
+thread1.Join();
+thread.Start();
+/*Hotel hotel = new Hotel("name", 4, "ars", "adfgas", 45);
 Hotel hotel1 = new Hotel("nam3", 6, "ars", "adfgas", 45);
 Hotel hotel2 = new Hotel("name6", 17, "ars", "adfgas", 45);
 
