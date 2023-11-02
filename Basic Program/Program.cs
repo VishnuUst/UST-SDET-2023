5a3edf678967205c12e4ac66a4b9589b79133db4
Vishnuust <vishnu.thulaseedharanpillai@ust.com>
2023-11-02 18:13:59 +0530

    Thread two

--- Basic Program/Program.cs
+++ Basic Program/Program.cs
@@ -2,18 +2,37 @@
 using Basic_Program;
 using System;
 using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        Warehouse warehouse = new Warehouse();
 
-class ThreadExample
+        Thread worker1 = new Thread(() => warehouse.AddBox(1));
+        Thread worker2 = new Thread(() => warehouse.AddBox(2));
+
+        worker1.Start();
+        worker2.Start();
+
+        worker1.Join();
+        worker2.Join();
+
+        Console.WriteLine("Work completed. Total boxes in the warehouse: " + warehouse.GetTotalBoxes());
+    }
+}
+
+/*class ThreadExample
 {
     static void Main()
     {
         // Create two threads
         Thread thread1 = new Thread(T1);
         Thread thread2 = new Thread(T1);
-
+        Thread thread3 = new Thread(T1);
         // Start the threads
         thread1.Start();
         thread2.Start();
+        thread3.Start();
 
         // Wait for the threads to finish
         thread1.Join();
---
+++ Basic Program/WareHouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+class Warehouse
+{
+    private int boxCount = 0;
+
+    public void AddBox(int workerId)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Thread.Sleep(100); // Simulate the time it takes to add a box
+            boxCount++;
+            Console.WriteLine($"Worker {workerId} added a box. Total boxes: {boxCount}");
+        }
+    }
+}
